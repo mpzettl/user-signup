4b12fca60ad99083429620f85e36e46b58784f5b
Peter Zettl <mpzettl@gmail.com>
2018-04-09 21:54:08 -0500

    setting up html forms and basic function

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import flask from Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
 @app.route("/")
-
 def index():
+    if input = True:
+        username = request.form("username")
+        return render_template("welcome.html", username = username)
+    else:
+        return render_template("edit.html")
 
-    return "Hello, world!"
+
 
 
 app.run()
