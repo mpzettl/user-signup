88d061f2dc7ab12c2649d172773e848a5ef067ba
Peter Zettl <mpzettl@gmail.com>
2018-04-28 21:04:53 -0500

    basic functionality finished

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
     
     
         
-    return render_template('edit.html')
+    return render_template('home.html')
     
 @app.route("/welcome", methods = ["POST", "GET"])
 def welcome():
@@ -27,39 +27,47 @@ def welcome():
     username_error = 'please do not use blank spaces'
     userlen_error = 'please enter a username between 3 and 20 characters'
     email_error = 'please reenter a valid email address'
-    password_error = 'invalid password length please reenter'
-    verify_error = 'please make sure your passwords match!'
+    password_error = 'please enter a password between 3 and 20 characters'
+    verify_error = 'please make sure your verification matches your password!'
     
     if username == "" or password =="" or verify =="":
 
-        return render_template('edit.html', fill_error=fill_error)
+        return render_template('home.html', fill_error=fill_error)
     
     elif  " " in username or " " in password:
 
-        return  render_template('edit.html', username_error=username_error)
+        return  render_template('home.html', username_error=username_error)
     
     elif len(username) < 3 or len(username) > 20:
 
-        return render_template('edit.html', userlen_error=userlen_error)
+        return render_template('home.html', userlen_error=userlen_error)
     
     elif len(password) < 3 or len(password) > 20:
 
-        return render_template('edit.html', password_error=password_error)
+        return render_template('home.html', password_error=password_error)
+    
+    elif len(email) <3 or len(email) > 20:
+
+        return render_template('home.html', email_error=email_error)
     
     elif "@" not in email:
 
-        return render_template('edit.html', email_error=email_error)
-        
+        return render_template('home.html', email_error=email_error)
+
+    elif "." not in email:
+
+        return render_template('home.html', email_error=email_error)
+
+    elif " " in email:
+        return render_template('home.html', email_error=email_error)
+
     elif password == verify:
     
         return render_template('welcome.html', username=username)
     
     else:
         
-        return render_template('edit.html', verify_error=verify_error)  
+        return render_template('home.html', verify_error=verify_error)  
 
-    
-
-
-
-app.run()
+if (__name__) == "__main__":
+    app.run()
