80ee5af53a7223104afc4fd5b0ba1aefdb624ad4
Peter Zettl <mpzettl@gmail.com>
2018-04-28 19:54:25 -0500

    working code for errors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, render_template
+import cgi
 
 app = Flask(__name__)
 
@@ -22,42 +23,42 @@ def welcome():
     password = request.form['password']
     verify = request.form['verify']
 
-    username_error = ''
-    email_error = ''
-    password_error = ''
-    verify_error = ''
+    fill_error = 'please fill in all areas'
+    username_error = 'please do not use blank spaces'
+    userlen_error = 'please enter a username between 3 and 20 characters'
+    email_error = 'please reenter a valid email address'
+    password_error = 'invalid password length please reenter'
+    verify_error = 'please make sure your passwords match!'
     
     if username == "" or password =="" or verify =="":
 
-        return 'please fill in all areas'
+        return render_template('edit.html', fill_error=fill_error)
     
     elif  " " in username or " " in password:
 
-        return username_error == 'please do not use blank spaces'
+        return  render_template('edit.html', username_error=username_error)
     
     elif len(username) < 3 or len(username) > 20:
 
-        return username_error == 'invalid name length please reenter'
+        return render_template('edit.html', userlen_error=userlen_error)
     
     elif len(password) < 3 or len(password) > 20:
 
-        return password_error == 'invalid password length please reenter'
+        return render_template('edit.html', password_error=password_error)
     
+    elif "@" not in email:
+
+        return render_template('edit.html', email_error=email_error)
+        
     elif password == verify:
     
-        return render_template('/welcome.html', username=username)
+        return render_template('welcome.html', username=username)
     
     else:
         
-        return verify_error == 'please make sure your passwords match!'
+        return render_template('edit.html', verify_error=verify_error)  
+
     
-   
-    if "@" + " " in email or " " + "@" in email:
-        return email_error == 'please reenter a valid email address'
-
-    elif "@" not in email:
-
-        return email_error == 'please reenter a valid email address'
 
 
 
